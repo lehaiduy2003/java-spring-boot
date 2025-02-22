1d38aadc62f91183bb82b2cf7347b1a5df756e62
Le Hai Duy <lehaiduy2003@gmail.com>
2025-02-22 11:49:33 +0700

    configure authentication

---
+++ src/main/java/com/example/onlinecourses/configs/AuthProviderConfig.java
@@ -0,0 +1,31 @@
+package com.example.onlinecourses.configs;
+
+import com.example.onlinecourses.services.Interfaces.IUserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthProviderConfig {
+
+    private final IUserService userService;
+
+    public AuthProviderConfig(IUserService userService) {
+        this.userService = userService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
