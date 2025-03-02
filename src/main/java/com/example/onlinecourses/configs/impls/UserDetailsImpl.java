0fdac8dc57ebb1173bb5555c53c277e80673227f
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-02 12:39:01 +0700

    Update path directory

---
+++ src/main/java/com/example/onlinecourses/configs/impls/UserDetailsImpl.java
@@ -0,0 +1,44 @@
+package com.example.onlinecourses.configs.impls;
+
+import com.example.onlinecourses.models.OauthProvider;
+import com.example.onlinecourses.models.Role;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private final Long id;
+    private final String email;
+    private final String phoneNumber;
+    private final String username;
+    private final String password;
+    private final boolean isActive;
+    private final Set<OauthProvider> oauthProviders;
+    private final Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority(role.getName()))
+            .toList();
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
