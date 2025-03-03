95cc5c60c6bc01d498075e84ebe6fa24e655aa8c
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-03 21:34:23 +0700

    Use custom annotation

--- src/main/java/com/example/onlinecourses/controllers/AuthController.java
+++ src/main/java/com/example/onlinecourses/controllers/AuthController.java
@@ -1,19 +1,19 @@
 package com.example.onlinecourses.controllers;
-import com.example.onlinecourses.dtos.responses.ApiResponse;
+import com.example.onlinecourses.dtos.auth.RefreshResponseDTO;
+import com.example.onlinecourses.dtos.responses.ApiResponseDTO;
 import com.example.onlinecourses.dtos.auth.AuthRequestDTO;
 import com.example.onlinecourses.dtos.auth.AuthResponseDTO;
 import com.example.onlinecourses.dtos.requests.post.UserCreationDTO;
 import com.example.onlinecourses.services.interfaces.IAuthService;
+import com.example.onlinecourses.swagger.v1.AuthSwagger;
 import com.example.onlinecourses.utils.CookieUtil;
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
@@ -27,41 +27,44 @@ public class AuthController {
     }
 
     @PostMapping("/sign-in")
-    public ResponseEntity<ApiResponse<AuthResponseDTO>> signIn(@RequestBody AuthRequestDTO authRequestDTO, HttpServletResponse response, HttpSession httpSession) {
+    @AuthSwagger.SignIn
+    public ResponseEntity<ApiResponseDTO<AuthResponseDTO>> signIn(@RequestBody AuthRequestDTO authRequestDTO, HttpServletResponse response, HttpSession httpSession) {
         AuthResponseDTO authResponseDTO = authService.signIn(authRequestDTO);
         // Set session attribute
         httpSession.setAttribute("userId", authResponseDTO.getUser().getId());
         CookieUtil.setCookies(response, REFRESH_TOKEN_COOKIE_NAME, authResponseDTO.getRefreshToken());
-        return ResponseEntity.status(200).body(new ApiResponse<>(true, "Sign in successful", authResponseDTO));
+        return ResponseEntity.status(200).body(new ApiResponseDTO<>(true, "Sign in successful", authResponseDTO));
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<ApiResponse<AuthResponseDTO>> signUp(@Valid @RequestBody UserCreationDTO userCreationDTO, HttpServletResponse response, HttpSession httpSession) {
+    @AuthSwagger.SignUp
+    public ResponseEntity<ApiResponseDTO<AuthResponseDTO>> signUp(@Valid @RequestBody UserCreationDTO userCreationDTO, HttpServletResponse response, HttpSession httpSession) {
         AuthResponseDTO authResponseDTO = authService.signUp(userCreationDTO);
         // Set session attribute
         httpSession.setAttribute("userId", authResponseDTO.getUser().getId());
         CookieUtil.setCookies(response, REFRESH_TOKEN_COOKIE_NAME, authResponseDTO.getRefreshToken());
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, "Sign up successful", authResponseDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseDTO<>(true, "Sign up successful", authResponseDTO));
     }
 
     @PostMapping("/sign-out")
-    public ResponseEntity<ApiResponse<Object>> signOut(@RequestAttribute(name = REFRESH_TOKEN_COOKIE_NAME) String refreshToken, HttpServletResponse response) {
-        // Delete refresh token cookie
+    @AuthSwagger.SignOut
+    public ResponseEntity<ApiResponseDTO<Nullable>> signOut(@RequestAttribute(name = REFRESH_TOKEN_COOKIE_NAME) String refreshToken, HttpServletResponse response) {
+        // revoke the refresh token
         authService.signOut(refreshToken);
         CookieUtil.deleteCookies(response, REFRESH_TOKEN_COOKIE_NAME);
-        return ResponseEntity.ok(new ApiResponse<>(true, "Sign out successful", null));
+        return ResponseEntity.ok(new ApiResponseDTO<>(true, "Sign out successful", null));
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<ApiResponse<Map<String, String>>> refreshToken(
+    @AuthSwagger.RefreshAccessToken
+    public ResponseEntity<ApiResponseDTO<RefreshResponseDTO>> refreshToken(
         @CookieValue(name = REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken) {
-        Map<String, String> responseBody = new HashMap<>();
         if (refreshToken == null) {
-            return ResponseEntity.status(401).body(new ApiResponse<>(false, "Refresh token not found", null));
+            return ResponseEntity.status(401).body(new ApiResponseDTO<>(false, "Refresh token not found", null));
         }
         String newAccessToken = authService.refreshAccessToken(refreshToken);
-        responseBody.put("accessToken", newAccessToken);
-        return ResponseEntity.ok(new ApiResponse<>(true, "Token refreshed successfully", responseBody));
+        RefreshResponseDTO refreshResponse = RefreshResponseDTO.builder().accessToken(newAccessToken).build();
+        return ResponseEntity.ok(new ApiResponseDTO<>(true, "Token refreshed successfully", refreshResponse));
     }
 
 }
