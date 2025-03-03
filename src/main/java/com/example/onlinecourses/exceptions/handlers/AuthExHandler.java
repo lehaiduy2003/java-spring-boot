f92c7cc16faabc9fd7b57125ce4a59396d551d00
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-03 21:33:44 +0700

    Change class name

--- src/main/java/com/example/onlinecourses/exceptions/handlers/AuthExHandler.java
+++ src/main/java/com/example/onlinecourses/exceptions/handlers/AuthExHandler.java
@@ -1,6 +1,6 @@
 package com.example.onlinecourses.exceptions.handlers;
 
-import com.example.onlinecourses.dtos.responses.ApiResponse;
+import com.example.onlinecourses.dtos.responses.ApiResponseDTO;
 import com.example.onlinecourses.exceptions.NoPermissionException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
@@ -15,22 +15,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AuthExHandler {
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<ApiResponse<String>> handleExpiredJwtException(ExpiredJwtException e) {
-        return ResponseEntity.status(401).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleExpiredJwtException(ExpiredJwtException e) {
+        return ResponseEntity.status(401).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<ApiResponse<String>> handleInvalidTokenException(MalformedJwtException e) {
-        return ResponseEntity.status(401).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleInvalidTokenException(MalformedJwtException e) {
+        return ResponseEntity.status(401).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(UnsupportedJwtException.class)
-    public ResponseEntity<ApiResponse<String>> handleUnsupportedJwtException(UnsupportedJwtException e) {
-        return ResponseEntity.status(401).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleUnsupportedJwtException(UnsupportedJwtException e) {
+        return ResponseEntity.status(401).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(NoPermissionException.class)
-    public ResponseEntity<ApiResponse<String>> handleNoPermissionException(NoPermissionException e) {
-        return ResponseEntity.status(403).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleNoPermissionException(NoPermissionException e) {
+        return ResponseEntity.status(403).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 }
