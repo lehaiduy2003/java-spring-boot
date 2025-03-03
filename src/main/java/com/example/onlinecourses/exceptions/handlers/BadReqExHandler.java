70f8f8e5aa58a6a5f84a1db3964f361ae202cd12
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-03 21:33:39 +0700

    Change class name

--- src/main/java/com/example/onlinecourses/exceptions/handlers/BadReqExHandler.java
+++ src/main/java/com/example/onlinecourses/exceptions/handlers/BadReqExHandler.java
@@ -1,6 +1,6 @@
 package com.example.onlinecourses.exceptions.handlers;
 
-import com.example.onlinecourses.dtos.responses.ApiResponse;
+import com.example.onlinecourses.dtos.responses.ApiResponseDTO;
 import com.example.onlinecourses.exceptions.PasswordNotMatchException;
 import com.example.onlinecourses.exceptions.ResourceAlreadyExistException;
 import com.example.onlinecourses.exceptions.ResourceNotFoundException;
@@ -17,42 +17,42 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class BadReqExHandler {
     @ExceptionHandler(PasswordNotMatchException.class)
-    public ResponseEntity<ApiResponse<String>> handlePasswordNotMatchException(PasswordNotMatchException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handlePasswordNotMatchException(PasswordNotMatchException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ApiResponse<String>> handleUsernameNotFoundException(UsernameNotFoundException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ApiResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ApiResponse<String>> handleIllegalStateException(IllegalStateException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleIllegalStateException(IllegalStateException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(ResourceAlreadyExistException.class)
-    public ResponseEntity<ApiResponse<String>> handleResourceAlreadyExistException(ResourceAlreadyExistException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleResourceAlreadyExistException(ResourceAlreadyExistException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<String>> handleResourceNotFoundException(ResourceNotFoundException e) {
-        return ResponseEntity.status(404).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(404).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(RoleNotFoundException.class)
-    public ResponseEntity<ApiResponse<String>> handleRoleNotFoundException(RoleNotFoundException e) {
-        return ResponseEntity.status(404).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleRoleNotFoundException(RoleNotFoundException e) {
+        return ResponseEntity.status(404).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ApiResponse<String>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
-        return ResponseEntity.status(400).body(new ApiResponse<>(false, e.getMessage(), null));
+    public ResponseEntity<ApiResponseDTO<String>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return ResponseEntity.status(400).body(new ApiResponseDTO<>(false, e.getMessage(), null));
     }
 }
