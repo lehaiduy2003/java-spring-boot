54b5dd256ccb172ccf09160621e9c821e568e373
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-07 18:31:27 +0700

    Remove @ToString to handle LazyInitializeException

--- src/main/java/com/example/onlinecourses/models/Chapter.java
+++ src/main/java/com/example/onlinecourses/models/Chapter.java
@@ -10,7 +10,6 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Entity
 @Table(name = "chapters")
 @Builder
--- src/main/java/com/example/onlinecourses/models/Question.java
+++ src/main/java/com/example/onlinecourses/models/Question.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Entity
 @Table(name = "questions")
 @Builder
