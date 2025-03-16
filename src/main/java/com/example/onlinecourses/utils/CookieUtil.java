baa652a875dcc21404e8d5a649eaceae4ce385dc
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-16 16:35:03 +0700

    Handle expire time for cookie

--- src/main/java/com/example/onlinecourses/utils/CookieUtil.java
+++ src/main/java/com/example/onlinecourses/utils/CookieUtil.java
@@ -6,22 +6,27 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
     private static final boolean IS_SECURE = Dotenv.load().get("ENV").equals("production"); // Set secure flag for cookies based on environment
+    private static final long JWT_REFRESH_EXPIRATION = Dotenv.load().get("JWT_REFRESH_EXPIRATION") != null ? Long.parseLong(Dotenv.load().get("JWT_REFRESH_EXPIRATION")) : 1000L * 60 * 60 * 24 * 7; // 7 days
     private CookieUtil() {
         throw new IllegalStateException("CookieUtil is a utility class");
     }
     public static void setCookies(HttpServletResponse response, String cookieName, String cookieValue) {
         // Set refresh token cookie
         Cookie refreshTokenCookie = new Cookie(cookieName, cookieValue);
+        int maxAge = Math.toIntExact(JWT_REFRESH_EXPIRATION / 1000); // convert milliseconds to seconds
         refreshTokenCookie.setHttpOnly(true); // prevent JavaScript from accessing the cookie
         refreshTokenCookie.setSecure(IS_SECURE);
-        refreshTokenCookie.setPath("/"); // only send to /api/v1/auth/refresh endpoint
+        refreshTokenCookie.setMaxAge(maxAge); // set the expiration time
+        refreshTokenCookie.setPath("/api/v1/auth/refresh"); // only send to /api/v1/auth/refresh endpoint
         response.addCookie(refreshTokenCookie);
     }
 
     public static void deleteCookies(HttpServletResponse response, String cookieName) {
         // Delete refresh token cookie
         Cookie cookie = new Cookie(cookieName, null);
+        cookie.setHttpOnly(true);
         cookie.setMaxAge(0);
+        cookie.setSecure(IS_SECURE);
         cookie.setPath("/");
         response.addCookie(cookie);
     }
