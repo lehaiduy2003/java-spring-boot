a832851bd6333f009c47615e0746e6912affc8d2
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-16 16:30:51 +0700

    Enable AspectJAutoProxy

--- src/main/java/com/example/onlinecourses/OnlineCoursesApplication.java
+++ src/main/java/com/example/onlinecourses/OnlineCoursesApplication.java
@@ -1,10 +1,12 @@
 package com.example.onlinecourses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableAspectJAutoProxy
 public class OnlineCoursesApplication {
 
 	public static void main(String[] args) {
