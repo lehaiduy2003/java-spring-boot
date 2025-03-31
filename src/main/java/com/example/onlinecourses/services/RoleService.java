1bcfb6e49a586901afc5c4de2fcda6a1d41af786
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-31 09:26:11 +0700

    implement checking permission on role method

--- src/main/java/com/example/onlinecourses/services/RoleService.java
+++ src/main/java/com/example/onlinecourses/services/RoleService.java
@@ -1,6 +1,7 @@
 package com.example.onlinecourses.services;
 
 import com.example.onlinecourses.exceptions.RoleNotFoundException;
+import com.example.onlinecourses.models.Permission;
 import com.example.onlinecourses.models.Role;
 import com.example.onlinecourses.repositories.RolesRepository;
 import com.example.onlinecourses.services.interfaces.IRoleService;
@@ -41,5 +42,25 @@ public class RoleService implements IRoleService {
         Role roleToUpdate = rolesRepository.findById(id).orElseThrow(() -> new RoleNotFoundException("Role not found"));
         roleToUpdate.setName(roleName);
         return rolesRepository.save(roleToUpdate);
+    }
+
+    /**
+     * Check if a role has a permission
+     * @param roles the roles to check (a user can have multiple roles)
+     * @param permission the permission to check if the role has it (a role can have multiple permissions)
+     * @return true if the role has the permission, false otherwise
+     */
+    @Override
+    public boolean isRolePermitted(Set<Role> roles, String permission) {
+        // loop through the roles
+        return roles.stream()
+            // get the permissions of each role
+            .map(Role::getPermissions)
+            // flatten the stream of permissions
+            .flatMap(Set::stream)
+            // get the name of each permission
+            .map(Permission::getName)
+            // check if the permission is equal to the permission we are checking
+            .anyMatch(permission::equals);
     }
 }
