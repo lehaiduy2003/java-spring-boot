a48b5303eb2805f0764c724d9e480a287d9d1e0c
Le Hai Duy <lehaiduy2003@gmail.com>
2025-03-31 09:22:43 +0700

    Exception for no illegal method args

---
+++ src/main/java/com/example/onlinecourses/exceptions/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.example.onlinecourses.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
